2b1e627d550f7400fc2cea9bdba13a69de9ab5fe
vadim_berezhnoy <vadim_berezhnoy@idpowers.com>
2023-08-25 21:16:28 +0300

    fix games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 from brain_games.cli import welcome_user
-from brain_games.scripts.brain_even import brain_even
 
 
 def main():
     print('Welcome to the Brain Games!')
-    name = welcome_user()
-    brain_even(name)
+    welcome_user()
 
 
 if __name__ == '__main__':
